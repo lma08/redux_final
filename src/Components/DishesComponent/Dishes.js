43377a4db32c21a56cca5fbf858895dbe1cb7f2d
lma08 <mlidzhieva@gmail.com>
2023-03-12 10:46:35 -0400

    first commit

---
+++ src/Components/DishesComponent/Dishes.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import dataDishes from "../../data/dataDishes";
+import { getSelectedCategory } from "../../redux/dishesSlice";
+import Dish from "./Dish";
+
+const Dishes = () => {
+
+    const selectedCategory = useSelector(getSelectedCategory);
+
+    return(
+        <div>
+            {dataDishes
+            .filter(dish => {
+                if (selectedCategory === 'ALL') return true;
+                return selectedCategory === dish.category;
+            })
+            .map((dish, id) => 
+                <Dish key={id} dish={dish}/>)}
+        </div>
+    )
+}
+
+export default Dishes;
---
+++ src/Components/Filters/Filter.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { filterCategory, getSelectedCategory } from "../../redux/dishesSlice";
+
+const Filter = ({category}) => {
+
+    const selectedCategory = useSelector(getSelectedCategory); 
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <p onClick={() => (dispatch(filterCategory(category)))} className={selectedCategory === category ? 'categoryButtonSelected categoryButton' : 'categoryButton'}>
+                {category}
+            </p>
+        </div>
+    )
+}
+
+export default Filter;
